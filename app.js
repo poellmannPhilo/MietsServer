5d69e9fb79fc1278f2ee18672fd368754785ff3a
Philo Pöllmann <philo.poellmann@xailabs.de>
2021-08-04 19:51:54 +0200

    setup connect to mongodb

--- app.js
+++ app.js
@@ -1,41 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const db = require('./db')
+const app = express()
+const apiPort = 8000
 
-var app = express();
+// Add headers
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.get('/', (req, res) => {
+    res.send('Welcome to the Express Backend. Everything works as expected for now.')
+})
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))

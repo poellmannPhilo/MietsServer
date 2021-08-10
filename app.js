50f476ecfd1fcb7e5d6f4765ad4a20841233b441
Philo Pöllmann <philo.poellmann@xailabs.de>
2021-08-10 19:57:35 +0200

    progress on routing

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
-const db = require('./db')
 const app = express()
-const apiPort = 8000
 
+app.use(express.json()) // for parsing application/json
+
+const models = require("./models");
+const db = require('./db')
+// routes
+
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
+
+const apiPort = 8000
 // Add headers
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Express Backend. Everything works as expected for now.')
+})
+
+app.post('/test', (req, res) => {
+    res.json(req.body);
 })
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
--- db/index.js
+++ db/index.js
@@ -1,11 +1,50 @@
 const mongoose = require('mongoose')
 
+const db = require("../models");
+const Role = db.role;
+
 mongoose
     .connect('mongodb://localhost:27017/miets_db', { useNewUrlParser: true })
+    .then(() => {
+        console.log('Successfully connected to MongoDB');
+        initial();
+    })
     .catch(e => {
         console.error('Connection error', e.message)
     })
 
-const db = mongoose.connection
+function initial() {
+    Role.estimatedDocumentCount((err, count) => {
+        if (!err && count === 0) {
+            new Role({
+                name: "user"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
 
-module.exports = db
+                console.log("added 'user' to roles collection");
+            });
+
+            new Role({
+                name: "moderator"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+
+                console.log("added 'moderator' to roles collection");
+            });
+
+            new Role({
+                name: "admin"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+
+                console.log("added 'admin' to roles collection");
+            });
+        }
+    });
+}
